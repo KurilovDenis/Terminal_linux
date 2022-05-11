bb1cdc29f09bc58469a8e5325084da7e7749ccda
KurilovDenis <yourgitemail@example.com>
2022-05-11 18:57:33 +0300

    script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+cd terminal_linux
+mkdir ww_1 ww_11 ww_22
+cd ww_1
+touch rr_1.txt rr_11.txt rr_22.txt jj_1.json jj_11.json
+mkdir  ff_1 ff_11 ff_22
+ls -la
+mv rr_22.txt rr_11.txt ff_11
+
